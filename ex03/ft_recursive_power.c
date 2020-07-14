799162d30d53eadb085a6c0145f033c91d59b293
Ryan <sinryan7@gmail.com>
2020-07-14 21:21:12 +0200

    Fourth commit

---
+++ ex03/ft_recursive_power.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+int	 ft_recursive_power(int nb, int power)
+{
+		if (power < 0)
+			return	(0);
+
+		if (power > 0)
+			return (nb * ft_recursive_power(nb, power - 1));
+		else 
+			return (1);	
+
+}
